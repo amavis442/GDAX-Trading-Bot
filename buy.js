4c75c6682874afc11e87cfbdfca2ae9e9e409cc3
Patrick Teunissen <p.teunissen18@gmail.com>
2018-06-07 16:07:14 +0200

    Added amount and limit price to calculate de number of orders needed

--- buy.js
+++ buy.js
@@ -7,13 +7,16 @@ let stdio = require('stdio');
 let authenticatedClient = null;
 let orderParams = null;
 let orderId = null;
+let diffPrice = 0.0;
 
 let ops = stdio.getopt({
     'cryptocurreny': {key: 'c', args: 1, mandatory: true, description: 'Which crypto curreny LTC,BTC, ETH or BCH'},
     'size': {key: 's',args: 1, mandatory: true, description: 'Size to buy per increment 0.1 ex. for 0.1 LTC'},
-    'number': {key: 'n', args: 1, mandatory: true, description: 'Number of orders'},
+    'number': {key: 'n', args: 1, description: 'Number of orders'},
     'price': {key: 'p', args: 1, mandatory: true, description: 'Start price to calculate'},
+    'limitprice': {key: 'l', args: 1, description: 'Limitprice (only used when number is not filled in and it is not included in the number of orders (n-1))'},
     'increment': {key: 'i', args: 1, mandatory: true, description: 'The increment in cents (5 = 5 cent increment)'},
+    'amount': {key: 'a', args: 1, description: 'Size of crypto you wonna sell (You do not need to fill in number and topprice'},
 });
 
 let CRYPTO_CURRENCY = ops.cryptocurreny + '-EUR';
@@ -21,11 +24,32 @@ let SIZE = parseFloat(ops.size);
 let NUMBER = parseInt(ops.number);
 let PRICE = parseFloat(ops.price);
 let INCREMENT = ops.increment;
+let LIMITPRICE = parseFloat(ops.limitprice);
+let AMOUNT = parseFloat(ops.amount);
 
 const PASSPHRASE = process.env.TRADING_BOT_PASSPHRASE;
 const KEY = process.env.TRADING_BOT_KEY;
 const SECRET = process.env.TRADING_BOT_SECRET;
 const GDAX_URI = 'https://api.gdax.com';
+
+if (!NUMBER && !LIMITPRICE && !AMOUNT){
+    console.log('Please fill in number of orders or the limitprice or the amount to buy......');
+    return;
+}
+
+if (!NUMBER && LIMITPRICE){
+    diffPrice = parseFloat((PRICE - LIMITPRICE) * 100).toFixed(2);
+    NUMBER = parseInt(Math.floor(diffPrice / INCREMENT));
+}
+
+// Round down so we do not sell
+if (!NUMBER && !LIMITPRICE){
+    NUMBER = parseInt(Math.floor(AMOUNT / SIZE));
+}
+
+console.log('Number: ' + NUMBER);
+return;
+
 
 const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
